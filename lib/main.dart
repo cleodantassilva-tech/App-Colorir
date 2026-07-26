c54b4793ad2c9e0f069c968c359a04633ff5a0e0
cleodantassilva-tech <cleodantas.silva@gmail.com>
2026-07-26 16:29:28 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/onboarding_screen.dart';
+import 'services/onboarding_service.dart';
 
 void main() {
   runApp(const AppColorir());
@@ -18,7 +20,26 @@ class AppColorir extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Roboto',
       ),
-      home: const HomeScreen(),
+      home: const _TelaInicialRoteador(),
+    );
+  }
+}
+
+class _TelaInicialRoteador extends StatelessWidget {
+  const _TelaInicialRoteador();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: OnboardingService.jaViu(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        return snapshot.data! ? const HomeScreen() : const OnboardingScreen();
+      },
     );
   }
 }
